e19e27b8e8e254c74254da3ae3f1c0aa55ffed73
Pyo-hogeun <37278812+Pyo-hogeun@users.noreply.github.com>
2026-04-21 12:27:13 +0900

    fix: use namespace import for cheerio in esm

--- backend/src/services/scraperService.js
+++ backend/src/services/scraperService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import crypto from 'crypto';
 import { env } from '../config/env.js';
 import { Shop } from '../models/Shop.js';
